a4b90d6c8a729e36f44974de64dcefc51cc5ecf0
Bo Anderson <mail@boanderson.me>
2024-01-09 02:18:42 +0000

    Add `fetch` method

--- lib/lazyjson/ruby_ext.rb
+++ lib/lazyjson/ruby_ext.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module LazyJSON
-  module DigMixin
+  module CollectionMixin
     def dig(*args)
       raise ArgumentError, "wrong number of arguments (given 0, expected 1+)" if args.empty?
 
@@ -15,14 +15,31 @@ module LazyJSON
         value.dig(*args)
       end
     end
+
+    def fetch(key, default = (default_omitted = true; nil))
+      if !default_omitted && block_given?
+        warn "warning: block supersedes default value argument"
+      end
+
+      value = self[key]
+      return value unless value.nil?
+
+      if block_given?
+        yield(key)
+      elsif default_omitted
+        raise KeyError, "key not found: #{key.inspect}"
+      else
+        default
+      end
+    end
   end
-  private_constant :DigMixin
+  private_constant :CollectionMixin
 
   class Array
-    include DigMixin
+    include CollectionMixin
   end
 
   class Hash
-    include DigMixin
+    include CollectionMixin
   end
 end
